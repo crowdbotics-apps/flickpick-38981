3f3b695226967bbea7b2c64b147ce75545a26b5f
Crowdbotics <team@crowdbotics.com>
2023-02-16 23:28:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flickpick-38981",
+  url: "https://flickpick-38981.botics.co",
+  api: "https://flickpick-38981.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
